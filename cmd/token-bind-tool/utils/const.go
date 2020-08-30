a24dbaba9ae4cf7a67194fc6d6778acc924e78dc
HaoyangLiu <lhy.0318@163.com>
2020-08-30 13:18:10 +0800

    implement bind tool

---
+++ cmd/token-bind-tool/utils/const.go
@@ -0,0 +1,27 @@
+package utils
+
+const (
+	Passwd            = "12345678"
+	NetworkType       = "network-type"
+	ContractData      = "contract-data"
+	Operation         = "operation"
+	BEP20ContractAddr = "bep20-contract-addr"
+
+	TransferBNB = "transferBNBAndDeployContract"
+	ApproveBind = "approveBindAndTransferOwnership"
+
+	Mainnet = "mainnet"
+	TestNet = "testnet"
+
+	BindKeystore = "bind_keystore"
+
+	TestnetRPC     = "https://data-seed-prebsc-1-s1.binance.org:8545"
+	TestnetChainID = 97
+
+	MainnnetRPC     = "https://bsc-dataseed1.binance.org:443"
+	MainnetChainID = 56
+
+	OneBNB          = 1e18
+	DefaultGasPrice = 20000000000
+	DefaultGasLimit = 4700000
+)
